9596317697cf4ac28fe75b172f69232bb4173294
Rarm Jet <rarm.jet@gmail.com>
2019-11-13 20:39:53 +1100

    passthrough log level

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,10 @@
 from main import app as application
+from core.config import LOG_LEVEL
+
+if LOG_LEVEL == "DEBUG":
+    DEBUG = True
+else:
+    DEBUG = False
 
 if __name__ == "__main__":
-    application.run()
+    application.run(debug=DEBUG, host="0.0.0.0", port=8080)
