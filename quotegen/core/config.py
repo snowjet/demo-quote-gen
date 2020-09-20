0d8925d739c953821ffdd9709d7ce58f075ca29b
rarm <rarm.jet@gmail.com>
2020-09-20 19:36:32 +1000

    remove wasted print statement

--- quotegen/core/config.py
+++ quotegen/core/config.py
@@ -33,7 +33,6 @@ if not DATABASE_URL:
 MAX_CONNECTIONS_COUNT = int(os.getenv("MAX_CONNECTIONS_COUNT", 10))
 MIN_CONNECTIONS_COUNT = int(os.getenv("MIN_CONNECTIONS_COUNT", 10))
 
-print(os.getenv("JSON_QUOTE_PATH", None))
 json_file_path = pathlib.Path.cwd().joinpath("quotes", "quotes.json")
 if os.getenv("JSON_QUOTE_PATH", None) is not None:
     test_env_path = pathlib.Path(os.getenv("JSON_QUOTE_PATH"))
