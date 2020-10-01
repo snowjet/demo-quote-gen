ae8658fb45ca8657adcdaf082ba63bb50a37e527
rarm <rarm.jet@gmail.com>
2020-10-01 19:37:26 +1000

    check if DB is sqlitle to remvoe same thread checking

--- quotegen/db/database.py
+++ quotegen/db/database.py
@@ -9,9 +9,15 @@ from core.log import logger
 """ for testing"""
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
-)
+if SQLALCHEMY_DATABASE_URL.startswith('sqlite'):
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL
+    )
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
