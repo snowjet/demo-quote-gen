e097689ea1cda8b9dda12f9315bc81afb18bcb11
rarm <rarm.jet@gmail.com>
2020-10-06 17:22:16 +1100

    choose api based on config

--- quotegen/crud/images.py
+++ quotegen/crud/images.py
@@ -10,60 +10,22 @@ class Image(BaseModel):
     name: Optional[str] = None
     detail: Optional[str] = None
 
-def get_image_simple(name):
+def get_image_simple(apiVersion: int = 1, name: str = None):
 
     try:
-        url = settings.external_image_api
+        if apiVersion == 2:
+            url = settings.external_image_api_v2
+            params={'author': name}
+        else:
+            url = settings.external_image_api
+            params={'name': name}
 
         with httpx.Client() as client:
             
             logger.debug("URL: " + url)
             logger.debug("Name: " + name)
 
-            response = client.get(url, params={'name': name})
-            response.raise_for_status()
-        try:
-            json_object = response.json()
-            if 'image' in json_object:
-                return {'image': json_object['image']}
-            else:
-                raise ValueError
-        except ValueError as e:
-            error_msg = "Not Valid JSON"
-            logger.error(error_msg)
-            return Image(name='error', Image=error_msg)
-
-    except httpx.RequestError as exc:
-        error_msg = f"An error occured while requesting {exc.request.url!r}."
-        logger.error(error_msg)
-        return Image(name='error', Image=error_msg)
-
-    except httpx.HTTPStatusError as exc:
-        error_msg = f"Error response {exc.response.status_code} while requesting {exc.request.url!r}."
-        logger.error(error_msg)
-        return Image(name='error', Image=error_msg)
-
-    except httpx.HTTPError as exc:
-        error_msg = f"HTTPError Error while requesting {exc.request.url!r}."
-        logger.error(error_msg)
-        return Image(name='error', Image=error_msg)
-
-    except httpx.InvalidURL as exc:
-        error_msg = f"Error while requesting {exc.request.url!r}."
-        logger.error(error_msg)
-        return Image(name='error', Image=error_msg)
-
-def get_image_simple_api_v2(name):
-
-    try:
-        url = settings.external_image_api_v2
-
-        with httpx.Client() as client:
-            
-            logger.debug("URL: " + url)
-            logger.debug("Name: " + name)
-
-            response = client.get(url, params={'author': name})
+            response = client.get(url, params=params)
             response.raise_for_status()
         try:
             json_object = response.json()
